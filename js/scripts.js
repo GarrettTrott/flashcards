9587004e6e6282162660faeb89ea94b249972d53
Student <example@example.com>
2019-10-22 16:45:05 -0700

    mess with the flashcard functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,42 @@
-$(document).ready(function() {
-  $("#javascript").click(function() {
-    $("#jsdefinition").slideDown()
-    $("#javascript").css({"cursor": "default"})
+$(document).ready(function(){
+$(".javascriptdiv").click(function(){
+  $("#jsdefinition").slideDown();
+  $("#javascript").css("cursor", "default");
+$(".javascriptdiv").click(function(){
+  $(".javascriptdiv").hide() ;
+  $(".operatorsdiv").show();
   });
-  $("#jsdefinition").click(function() {
-    $("#javascript").hide()
-    $("#jsdefinition").hide()
-    $("#operators").show()
-    $("#operators").css({"cursor": "pointer"})
+});
+$(".operatorsdiv").click(function(){
+  $("#operatorsdefinition").slideDown();
+  $("#operators").css("cursor", "default");
+$(".operatorsdiv").click(function(){
+  $(".operatorsdiv").hide() ;
+  $(".variablesdiv").show();
+});
+  $(".variablesdiv").click(function(){
+    $("#variablesdefinition").slideDown();
+    $("#variables").css("cursor", "default");
+  $(".variablesdiv").click(function(){
+    $(".variablesdiv").hide() ;
+    $(".functiondiv").show();
   });
-  $("#operators").click(function() {
-    $("#operatorsdefinition").slideDown()
-    $("#operators").css({"cursor": "default"})
+});
+  $(".functiondiv").click(function(){
+    $("#functiondefinition").slideDown();
+    $("#function").css("cursor", "default");
+  $(".functiondiv").click(function(){
+    $(".functiondiv").hide() ;
+    $(".methodsdiv").show();
+    });
+    $(".methodsdiv").slideDown();
+      $("#methods").css("cursor", "default");
+    $(".methodsdiv").click(function(){
+      $(".methodsdiv").hide() ;
+      $(".javascriptdiv").show();
+
+
+    });
   });
-  $("#operatorsdefinition").click(function() {
-    $("#operators").hide()
-    $("#operatorsdefinition").hide()
-    $("#variables").show()
-    $("#variables").css({"cursor": "pointer"})
-  });
-  $("#variables").click(function() {
-    $("#variablesdefinition").slideDown()
-    $("#variables").css({"cursor": "default"})
-  });
-  $("#variablesdefinition").click(function() {
-    $("#variables").hide()
-    $("#variablesdefinition").hide()
-    $("#function").show()
-    $("#function").css({"cursor": "pointer"})
-  });
-  $("#function").click(function() {
-    $("#functiondefinition").slideDown()
-    $("#function").css({"cursor": "default"})
-  });
-  $("#functiondefinition").click(function() {
-    $("#function").hide()
-    $("#functiondefinition").hide()
-    $("#methods").show()
-    $("#methods").css({"cursor": "pointer"})
-  });
-  $("#methods").click(function() {
-    $("#methodsdefinition").slideDown()
-    $("#methods").css({"cursor": "default"})
-  });
-  $("#methodsdefinition").click(function() {
-    $("#methods").hide()
-    $("#methodsdefinition").hide()
-    $("#javascript").show()
-    $("#javascript").css({"cursor": "pointer"})
-  });
+});
 });
