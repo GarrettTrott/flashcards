b61702fa4b376feb99191a3b8af18f7e86d81608
Student <example@example.com>
2019-10-22 11:27:34 -0700

    fix cursor problems

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,52 @@
 $(document).ready(function() {
   $("#javascript").click(function() {
     $("#jsdefinition").slideDown()
+    $("#javascript").css({"cursor": "default"})
   });
   $("#jsdefinition").click(function() {
     $("#javascript").hide()
     $("#jsdefinition").hide()
     $("#operators").show()
+    $("#operators").css({"cursor": "pointer"})
   });
   $("#operators").click(function() {
     $("#operatorsdefinition").slideDown()
+    $("#operators").css({"cursor": "default"})
   });
   $("#operatorsdefinition").click(function() {
     $("#operators").hide()
     $("#operatorsdefinition").hide()
     $("#variables").show()
+    $("#variables").css({"cursor": "pointer"})
   });
   $("#variables").click(function() {
     $("#variablesdefinition").slideDown()
+    $("#variables").css({"cursor": "default"})
   });
   $("#variablesdefinition").click(function() {
     $("#variables").hide()
     $("#variablesdefinition").hide()
     $("#function").show()
+    $("#function").css({"cursor": "pointer"})
   });
   $("#function").click(function() {
     $("#functiondefinition").slideDown()
+    $("#function").css({"cursor": "default"})
   });
   $("#functiondefinition").click(function() {
     $("#function").hide()
     $("#functiondefinition").hide()
     $("#methods").show()
+    $("#methods").css({"cursor": "pointer"})
   });
   $("#methods").click(function() {
     $("#methodsdefinition").slideDown()
+    $("#methods").css({"cursor": "default"})
   });
   $("#methodsdefinition").click(function() {
     $("#methods").hide()
     $("#methodsdefinition").hide()
     $("#javascript").show()
+    $("#javascript").css({"cursor": "pointer"})
   });
 });
